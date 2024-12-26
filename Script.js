e87bb3251516562b624009a69fc0f3fcbdf44291
Sagar Jain <149682589+sagarjain-109@users.noreply.github.com>
2024-12-26 23:31:24 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,66 @@
+let score = {
+  userScore: 0,
+  computerScore:0,
+  summary:'You are about to start',
+}
+display();
+
+
+function getRandom(){
+  let options = ['Paper','Rock','Scissor'];
+  return options[Math.floor(Math.random() *3)];
+}
+function reset(){
+  localStorage.clear();
+  display();
+}
+
+
+function display(){
+  let scorestring = localStorage.getItem('score');
+  score = JSON.parse(scorestring) || {
+    userScore: 0,
+    computerScore:0,
+    summary:'You are about to start',
+  }
+  document.querySelector('#userScore').innerText = score.userScore;
+  document.querySelector('#ComputerScore').innerText = score.computerScore;
+  document.querySelector('.summary').innerText = score.summary;
+}
+
+
+// function update(){
+//   let scorestring = localStorage.getItem('score');
+//   score = JSON.parse(scorestring) || {
+//     userScore: 0,
+//     computerScore:0
+//   }
+//   document.querySelector('#userScore').innerText = score.userScore;
+//   document.querySelector('#ComputerScore').innerText = score.computerScore;
+// }
+
+function clicked(message){
+  
+   let choice = getRandom();
+
+  if((message == 'Rock' && choice == 'Scissor')|| (message == 'Paper' &&  choice == 'Rock') || (message == 'Scissor' && choice == 'Paper')){
+     score.summary = `Your choice is ${message} and Computer choice is ${choice} So User Won`;
+    score.userScore++;
+  }
+  else if((message == 'Rock' && choice == 'Rock')|| (message == 'paper' &&  choice == 'Paper') || (message == 'Scissor' && choice == 'Scissor')){
+    score.summary = `Your choice is ${message} and Computer choice is ${choice} So Game Drawn`;
+  }
+  else{
+    score.summary = `Your choice is ${message} and Computer choice is ${choice} So Computer Won`;
+    score.computerScore++;
+  }
+  
+  
+  localStorage.setItem('score',JSON.stringify(score));
+  display();
+  
+  
+  
+}
+
+
